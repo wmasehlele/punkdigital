cf65e883f686f5198ad7eb4f28484144d934c5e7
William <wmasehlele@gmail.com>
2017-02-08 23:43:07 +0200

    updated email signature class now generating signaturure but not loading the logo

--- src/punk/devtest/EmailSignature.java
+++ src/punk/devtest/EmailSignature.java
@@ -2,22 +2,16 @@ package punk.devtest;
 
 import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JEditorPane;
+
+import org.apache.commons.io.FileUtils;
 
 public class EmailSignature implements SignatureInterface{
 	private String strNames;
@@ -62,26 +56,30 @@ public class EmailSignature implements SignatureInterface{
 		Random rand = new Random();
 		int tempid = rand.nextInt(1000) + 1;
 		String imageName = "image_"+tempid+".png";
-	    try {
-	    	InputStream in = new URL(this.strLogoUrl).openStream();
-	    	String sRootPath = System.getProperty("user.dir");
-			Files.copy(in, Paths.get("C:\\Users\\William\\workspace\\PunkDigital\\WebContent\\Images\\"+imageName));
+		File htmlTemplateFile = new File("C:\\Users\\William\\workspace\\signature.html");
+		File newHtmlFile = null;
+		String htmlString = "";
+		try {
+			htmlString = FileUtils.readFileToString(htmlTemplateFile);
+			htmlString = htmlString.replace("$names", this.strNames);
+			htmlString = htmlString.replace("$position", this.strPosition);
+			htmlString = htmlString.replace("$email", this.strEmail);
+			htmlString = htmlString.replace("$tell", this.strTell);
+			htmlString = htmlString.replace("$logo", this.strLogoUrl);
+			newHtmlFile = new File("C:\\Users\\William\\workspace\\new_signature.html");
+			FileUtils.writeStringToFile(newHtmlFile, htmlString);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}	
-        String html = "<html>"
-				+ "<head>"
-        		+ "</head></body><table><tr><td>"
-        		+ "<div style=\"padding:10px;float:left;border:1px solid black;width:200px;\\\">"
-	        		+ "<img src=\"C:/Users/William/workspace/PunkDigital/WebContent/Images/"+imageName+" class=\"img-responsive\\\" heigt=\"250px\\\" width=\"100px\\\">"
-        		+ "</div></td><td>"
-        		+ "<div style=\"padding:10px;float:right;color:blue;border:1px solid black;width:300px;margin-top:-100px;\\\">"
-	        		+ "<h2 style=\"text-align:right;\\\">"+this.strNames+"</h2>"
-	        		+ "<h2 style=\"text-align:right;\\\">"+this.strPosition+"</h2>"
-	        		+ "<h2 style=\"text-align:right;\\\">"+this.strTell +"</h2>"
-	        		+ "<h2 style=\"text-align:right;\\\">"+this.strEmail +"</h2>"
-        		+ "</div></td></tr></table>"
-        		+ "</body></html>";		
+		}
+		
+		
+		try {
+			htmlString = FileUtils.readFileToString(newHtmlFile);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+        String html = htmlString.toString();		
         int width = 700, height = 220;
         // Create a `BufferedImage` and create the its `Graphics`
         BufferedImage image_ = GraphicsEnvironment.getLocalGraphicsEnvironment()
@@ -102,7 +100,6 @@ public class EmailSignature implements SignatureInterface{
             e.printStackTrace();
         }
         System.out.println(imageName);		
-		
 		return imageName;
 	}
 }
